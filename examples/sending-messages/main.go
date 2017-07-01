5b28c425a7df6e4399f8515dc45aee8a85a258d4
Dimonchik0036 <dimonchik0036@gmail.com>
2017-07-01 14:05:47 +0700

    An example of sending messages has been added

---
+++ examples/sending-messages/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"github.com/dimonchik0036/vk-api"
+	"log"
+)
+
+func main() {
+	client, err := vkapi.DefaultClientFromLogin("<username>", "<password>", vkapi.ScopeMessages)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	client.Log(true)
+
+	if err := client.InitMyProfile([]string{}); err != nil {
+		log.Fatal(err)
+	}
+
+	if _, err := client.SendMessage(vkapi.NewMessage(client.User.Id, "Hello!")); err != nil {
+		log.Println(err)
+	}
+}
