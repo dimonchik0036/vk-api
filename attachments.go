6ae407c66907815f2e48ddcacf737d2e5513ed06
Dimonchik0036 <dimonchik0036@gmail.com>
2017-07-17 23:22:03 +0700

    Add video and document

--- attachments.go
+++ attachments.go
@@ -1,33 +1,37 @@
 package vkapi
 
 type Attachment struct {
-	Type      string `json:"type"`
-	AccessKey string `json:"access_key"`
-	Photo     *Photo `json:"photo"`
-	Audio     *Audio `json:"audio"`
+	Type      string    `json:"type"`
+	AccessKey string    `json:"access_key"`
+	Photo     *Photo    `json:"photo"`
+	Audio     *Audio    `json:"audio"`
+	Video     *Video    `json:"video"`
+	Document  *Document `json:"doc"`
 }
 
 type Photo struct {
-	ID      int64  `json:"id"`
-	AlbumID int64  `json:"album_id"`
-	OwnerID int64  `json:"owner_id"`
-	UserID  int64  `json:"user_id"`
-	Text    string `json:"text"`
-	Date    int64  `json:"date"`
-	Sizes   *[]struct {
-		Src    string `json:"src"`
-		Width  int    `json:"width"`
-		Height int    `json:"height"`
-		Type   string `json:"type"`
-	} `json:"sizes"`
-	Photo75   string `json:"photo_75"`
-	Photo130  string `json:"photo_130"`
-	Photo604  string `json:"photo_604"`
-	Photo807  string `json:"photo_807"`
-	Photo1280 string `json:"photo_1280"`
-	Photo2560 string `json:"photo_2560"`
-	Width     int    `json:"width"`
-	Height    int    `json:"height"`
+	ID        int64    `json:"id"`
+	AlbumID   int64    `json:"album_id"`
+	OwnerID   int64    `json:"owner_id"`
+	UserID    int64    `json:"user_id"`
+	Text      string   `json:"text"`
+	Date      int64    `json:"date"`
+	Sizes     *[]Sizes `json:"sizes"`
+	Photo75   string   `json:"photo_75"`
+	Photo130  string   `json:"photo_130"`
+	Photo604  string   `json:"photo_604"`
+	Photo807  string   `json:"photo_807"`
+	Photo1280 string   `json:"photo_1280"`
+	Photo2560 string   `json:"photo_2560"`
+	Width     int      `json:"width"`
+	Height    int      `json:"height"`
+}
+
+type Sizes struct {
+	Src    string `json:"src"`
+	Width  int    `json:"width"`
+	Height int    `json:"height"`
+	Type   string `json:"type"`
 }
 
 func (p *Photo) GetMaxSizePhoto() string {
@@ -65,4 +69,106 @@ type Audio struct {
 	GenreID  int64  `json:"genre_id"`
 	Date     int64  `json:"date"`
 	NoSearch int    `json:"no_search"`
+}
+
+type Document struct {
+	ID        int64  `json:"id"`
+	OwnerID   int64  `json:"owner_id"`
+	Title     string `json:"title"`
+	Size      int64  `json:"size"`
+	Ext       string `json:"ext"`
+	Url       string `json:"url"`
+	Date      int64  `json:"date"`
+	Type      int    `json:"type"`
+	AccessKey string `json:"access_key"`
+	Preview   struct {
+		Photo *struct {
+			Sizes *[]Sizes `json:"sizes"`
+		} `json:"photo"`
+		Graffiti *Graffiti `json:"graffiti"`
+		AudioMsg *AudioMsg `json:"audio_msg"`
+	} `json:"preview"`
+}
+
+func (doc *Document) IsTxt() bool {
+	return doc.Type == 1
+}
+
+func (doc *Document) IsArch() bool {
+	return doc.Type == 2
+}
+
+func (doc *Document) IsGif() bool {
+	return doc.Type == 3
+}
+
+func (doc *Document) IsImages() bool {
+	return doc.Type == 4
+}
+
+func (doc *Document) IsAudio() bool {
+	return doc.Type == 5
+}
+
+func (doc *Document) IsVideo() bool {
+	return doc.Type == 6
+}
+
+func (doc *Document) IsEBooks() bool {
+	return doc.Type == 7
+}
+
+func (doc *Document) IsUnknown() bool {
+	return doc.Type == 8
+}
+
+type Graffiti struct {
+	Src    string `json:"src"`
+	Width  int64  `json:"width"`
+	Height int64  `json:"height"`
+}
+
+type AudioMsg struct {
+	Duration int64   `json:"duration"`
+	Waveform []int64 `json:"waveform"`
+	LinkOgg  string  `json:"link_ogg"`
+	LinkMp3  string  `json:"link_mp3"`
+}
+
+type Video struct {
+	ID          int64  `json:"id"`
+	OwnerID     int64  `json:"owner_id"`
+	Title       string `json:"title"`
+	Description string `json:"description"`
+	Duration    int64  `json:"duration"`
+	Photo130    string `json:"photo_130"`
+	Photo320    string `json:"photo_320"`
+	Photo640    string `json:"photo_640"`
+	Photo800    string `json:"photo_800"`
+	Date        int64  `json:"date"`
+	AddingDate  int64  `json:"adding_date"`
+	Views       int64  `json:"views"`
+	Comments    int64  `json:"comments"`
+	Player      string `json:"player"`
+	AccessKey   string `json:"access_key"`
+	Processing  int    `json:"processing"`
+	Live        int    `json:"live"`
+	Upcoming    int    `json:"upcoming"`
+}
+
+func (v *Video) GetMaxPreview() string {
+	if v.Photo800 != "" {
+		return v.Photo800
+	}
+	if v.Photo640 != "" {
+		return v.Photo640
+	}
+	if v.Photo320 != "" {
+		return v.Photo320
+	}
+	if v.Photo130 != "" {
+		return v.Photo130
+	}
+
+	return ""
 }
